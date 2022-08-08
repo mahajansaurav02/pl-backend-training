6306151ad2d2733b0ab3f7f83cf6e7df69772274
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-08-08 14:48:52 +0530

    Assignment changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 const abc = require('../introduction/intro')
 const wel=require('../logger/logger.js')
 const help=require('../util/helper.js')
@@ -15,8 +16,15 @@ router.get('/test-me', function (req, res) {
     form.trim()
     form.lower()
     form.upper()
+    let arr1=["January",'February','March','April','May','Jun','July','August','September','October','November','December']
+    console.log(_.chunk(arr1,3))
+    let arr2=[1,3,5,7,9,11,13,15,17,19]
+    console.log(_.tail(arr2,9))
 
-
+    let arr3=_.union([1,2,3,4,5],[2,1,3,4,5,6,7],[9,7,5,4,7,4],[10,2,4,3],[9,8,11])
+    console.log(arr3)
+    let arr4=[["Company","Hyundai"],["car","verna"],["Model","V6"],["Colour","White"],["gear","Automatic"]]
+    console.log(_.fromPairs(arr4))
     res.send('My second ever api!')
 });
 
