748f52c9f37f6c44b097e6d153c483cdb33ebfca
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-08-09 01:44:24 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,29 +10,72 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+router.get('/movies', function (req, res){
+    let movies = ['Heropanti 2', 'Dhoom 3', 'Tere naam','Rab ne bana di jodi']
+    res.send(movies)
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+router.get('/movies/:indexNumber',function(req,res){
+    let movies = ['Heropanti 2', 'Dhoom 3', 'Tere naam','Rab ne bana di jodi','harry potter','fast and furious','mitwa']
+    let reqparam=req.params
+    let moviename=reqparam.indexNumber
+    for (let i in movies){
+        let num=movies[i]
+    if (moviename == [i]){
+       res.send(movies[i])
+      break;
+    }
     
-    res.send('Dummy response')
+}   
+    res.send("Error : Use a valid Index")
+}
+)
+
+router.get('/films',function(req,res){
+    let moviearr=[
+        {'id': 1,
+    'name': 'The mig'},
+        {'id': 2,
+    'name': 'Mission impossible 2'},
+        {'id': 3,
+    'name': 'The conjuring'},
+        {'id': 4,
+    'name': 'vikings'},
+        {'id': 5,
+    'name': 'Game of thrones'}
+]
+let arrresult=[]
+for(let i=0;i<moviearr.length;i++){
+    let main=moviearr[i];
+if(i==main.id){
+    let store=
+}
+}
+ req.send()
 })
+
+
+
+
+
+// router.get('/student-details/:name', function(req, res){
+//     /*
+//     params is an attribute inside request that contains 
+//     dynamic values.
+//     This value comes from the request url in the form of an 
+//     object where key is the variable defined in code 
+//     and value is what is sent in the request
+//     */
+
+//     let requestParams = req.params
+
+//     // JSON strigify function helps to print an entire object
+//     // We can use any ways to print an object in Javascript, JSON stringify is one of them
+//     console.log("This is the request "+ JSON.stringify(requestParams))
+//     let studentName = requestParams.name
+//     console.log('Name of the student is ', studentName)
+    
+//     res.send('Dummy response')
+// })
 
 module.exports = router;
