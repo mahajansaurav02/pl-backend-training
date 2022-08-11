8cc797ae93000b468cc3591704647611c1bc181e
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-08-11 21:24:05 +0530

    assignment add

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const myHelper = require('../util/helper')
-const underscore = require('underscore')
+const underscore = require('underscore');
+const { result } = require('underscore');
 
 const router = express.Router();
 
@@ -46,9 +47,76 @@ let playrearr=[
 
 router.post('/players-post',function(req,res){
     let playersob=req.body
-    // console.log(playersob)
-    playrearr.push(playersob)
-    res.send(playrearr)
+    let playerName=playersob.name
+     let isNamedouble = false
+
+    for (let i=0;i<playrearr.length;i++){
+        let Player=playrearr[i]
+        if(Player.name==playerName){
+            isNamedouble= true
+            break;
+        }
+    }
+    if (isNamedouble){
+        res.send("player is already exist")
+    } else{
+        playrearr.push(playersob)
+        res.send(playrearr)
+    }
+   
+})
+let peopleList=[
+{
+    name: "PK",
+    age: 10,
+    gender: "male",
+    Votingstatus:false
+  
+    },
+{
+    name: "SK",
+    age: 20,
+    gender: "male",
+    Votingstatus:false
+  
+    },
+{
+    name: "AA",
+    age: 70,
+    gender: "male",
+    Votingstatus:false
+  
+    },
+{
+    name: "SC",
+    age: 5,
+    gender: "male",
+    Votingstatus:false
+  
+    },
+{
+    name: "HO",
+    age: 40,
+    gender: "male",
+    Votingstatus:false
+  
+    },
+
+]
+router.post('/voting-post',function(req,res){
+    let querypar=req.query.input;
+    let finalarr=[]
+    for(let i=0;i<peopleList.length;i++){
+
+        let list=peopleList[i]
+        if (list.age>=querypar){
+           list.Votingstatus=true
+
+           finalarr.push(peopleList[i])
+          
+        }
+    }   
+  res.send({data:finalarr,status:true})
 })
 
 module.exports = router;
