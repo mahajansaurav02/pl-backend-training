f0c09b1f206d05fd6a0e9fe63ab46c53b26ce110
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-08-12 16:29:45 +0530

    Mongoose update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+
+const route =require('./routes/route.js');
+const{default: mongoose}=require('mongoose')
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://mahajansaurav02:N0CMKHEIpsdz20yh@cluster0.cfjop8w.mongodb.net/?retryWrites=true&w=majority"
+,{
+    useNewUrlParser:true
+}
+).then(()=>{console.log("MongoDb is connet")})
+.catch(err=>console.log(err))
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
